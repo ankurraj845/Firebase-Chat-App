52d67bb6b7e6a8b7f7d6f8e2e2461e632c34c523
ankurraj845 <ankurraj643@gmail.com>
2022-08-30 15:20:49 +0530

    there is issue in chat_screen somewhere around Stream builder (line 11)

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -33,7 +33,7 @@ signin(String email, password) async {
     print('/////// Success ////////');
   } on FirebaseAuthException catch (e) {
     if (e.code == 'user-not-found') {
-      print('No user found for that Email.');
+      print('No user found for that email.');
     } else if (e.code == 'wrong-password') {
       print('Wrong password provided for that user.');
     }
